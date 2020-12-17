8823f0face7c6b7b668feeb377d41102f126c4b3
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2020-12-17 16:38:10 +0100

    esercizio revisionato

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-
+//devo contare da 1 a 100 ---> uso il for
 for (var i = 1; i <= 100; i++) {
 
-  if ((i % 3) === 0) {
-    console.log([i] + ' Fizz');
-  } else if ((i % 5) === 0) {
-    console.log([i] + ' Buzz');
+//prima condizione che si deve verificare è che se un numero è multiplo si di 3 che di 5 deve scrivere FizzBuzz
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log( 'FizzBuzz');
+
+  } else if (i % 5 == 0) { // seconda condizione è che se un numero è multiplo solo di 5 deve scrivere Buzz
+    console.log('Buzz');
+
+  } else if (i % 3 == 0) {// Terza condizione è che  se un numero è multiplo di 3 deve scrivere Fizz
+    console.log('Fizz');
+
+  } else { // ultima condizione è scrivere i numeri rimanenti in numero
+    console.log(i);
   }
 
-  if (((i % 3) == 0) && ((i % 5) == 0)){
-    console.log([i] + ' FizzBuzz');
-  } else {
-    console.log([i]);
-  }
 }
